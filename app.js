e26d87070d149f4635ddad70f7569d500f3fd223
Shanmuga <notifyshan@gmail.com>
2018-07-05 16:46:16 +1000

    HTTP response code updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello AIDA - Test service');
+    res.writeHead(202, {'Content-Type': 'text/plain'});
+    res.end('Hello AIDA - Test service In Progress....');
 }).listen(8080);
