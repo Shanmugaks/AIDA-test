e62a7afbce938a83a02582cced3464e9b69174af
Shanmuga <notifyshan@gmail.com>
2018-07-08 20:55:35 +1000

    second baseline

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(202, {'Content-Type': 'text/plain'});
-    res.end('Hello AIDA - Test service In Progress.... 8th July 2018 at 8:23PM');
-}).listen(8080);
+exports.handler = function(event, context, callback) 
+{
+    var error = null;
+    var NoError = null;
+	
+	var result = MyLambdaFunction ();
+ 
+	
+	callback(error, result);
+}
+ 
+function MyLambdaFunction (InputParams) 
+{
+	// MyLambdaFunction logic here
+	var Result = 'My Lambda Tester';
+	return Result;
+}
